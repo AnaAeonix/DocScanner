c7433178f42cd6ec6311e6197b6b0881955ff1b3
AnaAeonix <165149396+AnaAeonix@users.noreply.github.com>
2024-04-02 15:48:21 +0530

    cam_ui changes

---
+++ camDes1main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from camDes1 import Ui_MainWindow
+
+class MyMainWindow(QMainWindow):
+    def __init__(self):
+        super(MyMainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+def main():
+    app = QApplication(sys.argv)
+    window = MyMainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
